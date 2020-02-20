db80e10185a558cc14503aaa91eb5d3dfce36c36
ampboris <boris@wpcpro.com>
2020-02-20 15:33:53 +1100

    Delete Calculator.java

--- src/main/java/com/interview/ee/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.interview.ee;
-
-import java.util.List;
-
-public abstract class Calculator {
-    public abstract String getCalculatorType();
-    public abstract int getPaymentAmount(List<ProductItem> items);
-}
